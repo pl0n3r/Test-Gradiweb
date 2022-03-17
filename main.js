94be8b0dcd20e996a2eaee15fade62f1eeecd341
Felipe <franciscof.suarez@vasslatam.com>
2022-03-16 19:04:35 -0500

    Js y Css añadidos

---
+++ main.js
@@ -0,0 +1,62 @@
+Vue.createApp({
+    data() {
+      return {
+        info: null,
+        nombre: null,
+        img: null,
+        real: null,
+        alto: null,
+        descp: null,
+        gal: null,
+        variacion: null
+      }
+    },
+    mounted () {
+    axios
+      .get('https://graditest-store.myshopify.com/products/free-trainer-3-mmw.js')
+      .then(response => {
+        this.info = response.data.id
+        this.nombre = response.data.title
+        this.img = response.data.featured_image
+        this.real = response.data.price
+        this.alto = response.data.price_max
+        this.descp = response.data.description
+        this.gal = response.data.images
+        this.variacion = response.data.variants
+        console.log(response.data)
+    })
+      .catch(error => console.log(error))
+      
+      
+  },
+    computed:{
+        juntos: function()
+        {
+            return this.nombre+" " + this.apellido
+            
+        },
+        
+        mayus: function ()
+        {
+            return this.nombre.toUpperCase() + " " + this.apellido.toUpperCase()
+        
+        },
+            },
+    methods: {
+        log(){
+            console.log('1 Año se ha añadido')
+            this.edad= parseInt(this.edad) + 1
+        },
+        restar(){
+          console.log('1 Año se ha restado')
+          this.edad= parseInt(this.edad) - 1
+        },
+        saludar(name, event ){
+          console.log('Hola', name)
+          console.log(event.target)
+        }
+        
+    }
+  
+}
+  ).mount('#app')
